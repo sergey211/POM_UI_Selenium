3dc0a95f09f63fa39f2c8d2e62762520bff1c14e
Sergey <sergey211@gmail.com>
2023-10-01 15:05:36 +0200

    chrome_options_added

--- conftest.py
+++ conftest.py
@@ -10,6 +10,10 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 
 @pytest.fixture()
